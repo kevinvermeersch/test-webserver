e09661636d942d8e90cbabeeee72dc8a2d2e72ba
Kevin Vermeersch <kevinvermeersch@github>
2012-11-02 17:12:08 +0100

    change file location of _index.html

--- node/app2.js
+++ node/app2.js
@@ -5,7 +5,7 @@ http.createServer(function(req,res){
 	
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	
-	fs.readFile('_index.html',function(err,contents){
+	fs.readFile('../_index.html',function(err,contents){
 
 		res.end(contents);
 		console.log("file read");
